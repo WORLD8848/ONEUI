16040d2ec0c01335805cc0cacbaf51dc1a744843
SH KIM <world8848@naver.com>
2021-11-18 00:07:07 +0900

    Init

---
+++ app/build/generated/source/buildConfig/debug/com/buxuan/baseoneui/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.buxuan.baseoneui;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.buxuan.baseoneuiapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 49;
+  public static final String VERSION_NAME = "1.00.00";
+}
